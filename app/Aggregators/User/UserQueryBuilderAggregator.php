2c3b4dd1e5602f1323d14e6e324e2837650b2f30
Yusif Zourab <wordlearningpress@gmail.com>
2022-01-03 14:34:58 +0200

    Don't show in users list, accounts with profile_display_mode === 0 and friends. #63

--- app/Aggregators/User/UserQueryBuilderAggregator.php
+++ app/Aggregators/User/UserQueryBuilderAggregator.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Builder;
 class UserQueryBuilderAggregator
 {
     private User | Builder $builder;
-    
+
     public function __construct(array $relations = [], array $withCounts = [])
     {
         $this->builder = User::query()
@@ -19,5 +19,24 @@ class UserQueryBuilderAggregator
     public function getBuilder(): Builder
     {
         return $this->builder;
+    }
+
+    public function byNotClosedProfilesInPrivacySettings(): static
+    {
+        $this->builder->whereHas('privacySettings', function (Builder $builder) {
+            $builder
+                ->select(['id', 'profile_display_mode'])
+                ->where('profile_display_mode', 1);
+        });
+
+        return $this;
+    }
+
+    public function byNotFriendsFor(User $user): static
+    {
+        $this->builder
+            ->whereNotIn('id', $user->friends()->pluck('friend_id'));
+
+        return $this;
     }
 }

a9168bf57637fac4a3e4e404f45567c275f0ebd8
Yusif Zourab <wordlearningpress@gmail.com>
2022-04-03 17:01:18 +0300

    Task-16 - Optimized users response.

--- app/Http/Resources/User/UserCollection.php
+++ app/Http/Resources/User/UserCollection.php
@@ -3,17 +3,43 @@
 namespace App\Http\Resources\User;
 
 use App\Http\Resources\BaseCollection;
+use Illuminate\Http\Resources\MissingValue;
+use Illuminate\Pagination\AbstractCursorPaginator;
+use Illuminate\Pagination\AbstractPaginator;
+use Illuminate\Support\Collection;
 
 class UserCollection extends BaseCollection
 {
+    /**
+     * @param mixed $resource
+     * @return mixed
+     */
+    public function collectResource($resource)
+    {
+        if ($resource instanceof MissingValue) {
+            return $resource;
+        }
+
+        if (is_array($resource)) {
+            $resource = new Collection($resource);
+        }
+
+        $this->collection = $resource->toBase();
+
+        return ($resource instanceof AbstractPaginator || $resource instanceof AbstractCursorPaginator)
+            ? $resource->setCollection($this->collection)
+            : $this->collection;
+    }
+
     public function toArray($request): array
     {
-        return parent::toArray($this->collection);
-//        return $this->collection->map(function (UserResource $user) use ($request) {
-//            return $user->setAdditionalData(collect([
-//                'isMyFriendsIDs' => $this->getMyFriendsIDs()
-//            ]))->toArray($request);
-//        })->toArray();
+        $additionalData = collect([
+            'isMyFriendsIDs' => $this->getMyFriendsIDs()
+        ]);
+
+        return $this->collection->map(function ($user) use ($additionalData) {
+            return new UserResource($user, $additionalData);
+        })->toArray();
     }
 
     private function getMyFriendsIDs(): array
@@ -21,18 +47,6 @@ class UserCollection extends BaseCollection
         return $this
             ->authUser
             ->hasFriends($this->collection->pluck('id')->toArray())
-            ->toArray();
-    }
-
-    private function getDialogsIDs(): array
-    {
-        return $this
-            ->authUser
-            ->dialogs()
-            ->whereHas('users', function ($builder) {
-                $builder->where('user_id', $this->collection->pluck('id')->toArray());
-            })
-            ->pluck('id')
             ->toArray();
     }
 }
